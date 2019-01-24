410c0cbb208e03023eae42d0efd89eab01156b70
Joffry Ferrater <joffry.ferrater@axiomatics.com>
2019-01-24 14:51:13 +0100

    make gradlew executable

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+echo "Building resource-models"
+cd resource-models && ./gradlew clean build publishToMavenLocal && cd ..
+echo "Building medical-records-service"
 cd medical-records-service && ./gradlew clean build && cd ..
+echo "Building patient-service"
 cd patient-service && ./gradlew clean build && cd ..
+cd health-care-app && ./gradlew clean build && cd ..
